566d54ab1226de53604f9e0fa238b35dd236d0da
Roman <voytsekhivskyi@mail.com>
2020-02-08 20:38:56 +0100

    added copy to clipboard button

---
+++ src/js/copytocb.js
@@ -0,0 +1,46 @@
+	function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  textArea.style.position="fixed";  //avoid scrolling to bottom
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
+var ds = document.querySelector('.ds'),
+  tl = document.querySelector('.tl'),
+  em = document.querySelector('.em')
+
+ds.addEventListener('click', function(event) {
+  copyTextToClipboard('discord');
+});
+
+
+tl.addEventListener('click', function(event) {
+  copyTextToClipboard('telegram');
+});
+
+em.addEventListener('click', function(event) {
+  copyTextToClipboard('e-mail');
+});
