e5056e91f78df6b045f916a21984ad0c187b043e
Roman <voytsekhivskyi@mail.com>
2020-02-09 20:19:03 +0100

    fixed buttons bugs

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,5 +1,17 @@
-document.getElementById('tl').onclick = function changeContent() {
+function changeContenttl() {
+    document.getElementById('tl').innerHTML = "Copied";    
+    document.getElementById('tl').style.cssText = 'background-color: #43B581;';
+    setTimeout(() => { document.getElementById('tl').innerHTML = "Copy";  document.getElementById('tl').style.cssText = 'background-color: #333;';  }, 1500);
+}
 
-    document.getElementById('tl').innerHTML = "Copy";
+function changeContentds() {
+    document.getElementById('ds').innerHTML = "Copied";    
+    document.getElementById('ds').style.cssText = 'background-color: #43B581;';
+    setTimeout(() => { document.getElementById('ds').innerHTML = "Copy";  document.getElementById('ds').style.cssText = 'background-color: #333;';  }, 1500);
+}
 
- }
+function changeContentem() {
+    document.getElementById('em').innerHTML = "Copied";    
+    document.getElementById('em').style.cssText = 'background-color: #43B581;';
+    setTimeout(() => { document.getElementById('em').innerHTML = "Copy";  document.getElementById('em').style.cssText = 'background-color: #333;';  }, 1500);
+}
