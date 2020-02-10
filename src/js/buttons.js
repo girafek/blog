f8bee316677c9613c9ee478ad1e8e15bd48032d5
Roman <voytsekhivskyi@mail.com>
2020-02-10 20:47:40 +0100

    fixed button background color bug

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,17 +1,17 @@
 function changeContenttl() {
     document.getElementById('tl').innerHTML = "Copied";    
     document.getElementById('tl').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('tl').innerHTML = "Copy";  document.getElementById('tl').style.cssText = 'background-color: #333;';  }, 1500);
+    setTimeout(() => { document.getElementById('tl').innerHTML = "Copy";  document.getElementById('tl').style.cssText = 'background-color: #305CF8;';  }, 1500);
 }
 
 function changeContentds() {
     document.getElementById('ds').innerHTML = "Copied";    
     document.getElementById('ds').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('ds').innerHTML = "Copy";  document.getElementById('ds').style.cssText = 'background-color: #333;';  }, 1500);
+    setTimeout(() => { document.getElementById('ds').innerHTML = "Copy";  document.getElementById('ds').style.cssText = 'background-color: #305CF8;';  }, 1500);
 }
 
 function changeContentem() {
     document.getElementById('em').innerHTML = "Copied";    
     document.getElementById('em').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('em').innerHTML = "Copy";  document.getElementById('em').style.cssText = 'background-color: #333;';  }, 1500);
+    setTimeout(() => { document.getElementById('em').innerHTML = "Copy";  document.getElementById('em').style.cssText = 'background-color: #305CF8;';  }, 1500);
 }
