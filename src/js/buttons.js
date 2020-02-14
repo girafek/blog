5428efbc94c67352a3ff4d1a5ff1e8e47212451f
Roman <voytsekhivskyi@mail.com>
2020-02-14 13:40:28 +0100

    fixed copy to clipboard tooltip bug

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,17 +1,7 @@
-function changeContenttl() {
-    document.getElementById('tl').innerHTML = "Copied";    
-    document.getElementById('tl').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('tl').innerHTML = "Copy";  document.getElementById('tl').style.cssText = 'background-color: #305CF8;';  }, 1500);
-}
-
-function changeContentds() {
-    document.getElementById('ds').innerHTML = "Copied";    
-    document.getElementById('ds').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('ds').innerHTML = "Copy";  document.getElementById('ds').style.cssText = 'background-color: #305CF8;';  }, 1500);
-}
-
 function changeContentem() {
-    document.getElementById('em').setAttribute(data-tooltip, value: Copied);    
-    document.getElementById('em').style.cssText = 'background-color: #43B581;';
-    setTimeout(() => { document.getElementById('em').innerHTML = "Copy";  document.getElementById('em').style.cssText = 'background-color: #305CF8;';  }, 1500);
+    var elem = document.getElementById("em");
+    var attrs = elem.attributes;
+    attrs["data-tooltip"].value = "Copied!";
+    setTimeout(() => { attrs["data-tooltip"].value = "Click to copy"; }, 1500);
 }
+
