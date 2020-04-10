7c3b1c9782189174b785824a1db8b981bcecab91
Roman Wojciechowski <voytsekhivskyi@mail.com>
2020-04-10 17:34:23 +0200

    added new design

--- src/js/buttons.js
+++
@@ -1,7 +0,0 @@
-function changeContentem() {
-    var elem = document.getElementById("em");
-    var attrs = elem.attributes;
-    attrs["data-tooltip"].value = "Copied!";
-    setTimeout(() => { attrs["data-tooltip"].value = "Click to copy"; }, 1500);
-}
-
